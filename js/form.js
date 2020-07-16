cb45b0305c0f6f83034b89c79cb88f6bfbaedfd3
groserVl <59569597+groserVl@users.noreply.github.com>
2020-07-16 13:49:52 +0300

    сделаны небольшие поправки в коде

--- js/form.js
+++ js/form.js
@@ -166,8 +166,7 @@
     window.load.resetImages();
     removePins();
     adForm.reset();
-    pricesForm.min = minPricesOfTypes[typeForm.value];
-    pricesForm.placeholder = minPricesOfTypes[typeForm.value];
+    setMinPricesOfTypes();
     inputAddressForm.value = getAddressForm(MAP_PIN_X, MAP_PIN_Y);
     mapPinMain.style.left = MAP_PIN_X + 'px';
     mapPinMain.style.top = MAP_PIN_Y + 'px';
